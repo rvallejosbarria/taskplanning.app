78de0e8fdf3a091fb9d93c4ebc0db9ab1a564325
Roberto Vallejos Barria <rvallejos@puertoangamos.cl>
2024-07-10 12:08:13 -0400

    chore: send request to create a new task to api

--- app/api/tasks/new/route.js
+++ app/api/tasks/new/route.js
@@ -1,22 +1,29 @@
 export const POST = async (req) => {
   const data = await req.json()
 
-  const response = await fetch('http://localhost:5072/api/tasks', {
+  const employees = data.employees
+
+  employees.forEach((employee, idx) => {
+    if (idx === 0) {
+      employee.role = "Leader"
+    } else {
+      employee.role = "Helper"
+    }
+  })
+
+  const response = await fetch('http://10.200.72.21/TaskPlanningAPI/api/Tasks', {
     method: 'POST',
     body: JSON.stringify({
       description: data.description,
       creator: "rvallejos",
       status: data.status,
-      importance: data.importance
+      importance: data.importance,
+      taskEmployees: employees
     }),
     headers: {
       'Content-Type': 'application/json'
     }
   })
-
-  const message = await response.text()
-
-  console.log(message)
 
   if (!response.ok) {
     return new Response("Failed to create a new task", { status: 500 })
