1b272bc105bfa24031ba0395140b1ba7a207f518
Roberto Vallejos Barria <rvallejos@puertoangamos.cl>
2024-07-10 11:57:29 -0400

    chore: fetch data from api

---
+++ app/api/employees/route.js
@@ -0,0 +1,11 @@
+export const GET = async (req) => {
+  const response = await fetch('http://10.200.72.21/TaskPlanningAPI/api/Employees')
+
+  if (!response.ok) {
+    return new Response("Failed to fetch all employees", { status: 500 })
+  }
+
+  const employees = await response.json()
+
+  return new Response(JSON.stringify(employees), { status: 201 })
+}
---
+++ app/api/tasks/route.js
@@ -0,0 +1,11 @@
+export const GET = async (req) => {
+  const response = await fetch('http://10.200.72.21/TaskPlanningAPI/api/Tasks')
+
+  if (!response.ok) {
+    return new Response("Failed to fetch all tasks", { status: 500 })
+  }
+
+  const tasks = await response.json()
+
+  return new Response(JSON.stringify(tasks), { status: 201 })
+}
